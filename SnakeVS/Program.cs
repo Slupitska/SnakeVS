ffbe70f0da73872fde0d173eedf9ab41b9bbb38f
Nat Sl <slupitska@gmail.com>
2022-06-20 10:20:18 +0300

    snake, lessons

---
+++ SnakeVS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char syml = '*';
+            Console.SetCursorPosition(x1; y1);
+            Console.WriteLine(sym1);
+
+            Console.Readline();
+        }
+    }
+}
